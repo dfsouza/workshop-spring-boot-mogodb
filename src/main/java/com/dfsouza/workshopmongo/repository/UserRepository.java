907f8a0f29a78c1911b2409336960a0c576f4463
Daniel Souza <danielfsouza@gmail.com>
2021-06-14 21:38:51 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/dfsouza/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.dfsouza.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dfsouza.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
