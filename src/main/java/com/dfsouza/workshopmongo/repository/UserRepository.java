956ccb7f443c55b83f90d575bda29b6696f48bda
Daniel Souza <danielfsouza@gmail.com>
2021-06-15 22:25:03 -0300

    Obtendo usuário por id

--- src/main/java/com/dfsouza/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/dfsouza/workshopmongo/repository/UserRepository.java
@@ -8,4 +8,5 @@ import com.dfsouza.workshopmongo.domain.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
+	
 }
---
+++ src/main/java/com/dfsouza/workshopmongo/resources/exeption/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.dfsouza.workshopmongo.resources.exeption;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.dfsouza.workshopmongo.resources.exception.StandardError;
+import com.dfsouza.workshopmongo.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);		
+	}
+}
